8c7a667e7214012914266004f36a28ef113b037c
Filippa <filippa.steiner@icloud.com>
2026-09-05 23:06:59 +0200

    p

--- src/pages/Fastily.tsx
+++ src/pages/Fastily.tsx
@@ -7,7 +7,6 @@ import image2 from '../assets/fastily/Logo.png'
 function Fastily() {
     return (
         <div className="relative mt-[100px] mb-5">
-
             <Link
                 to="/"
                 className="absolute -left-12 top-1"
@@ -17,12 +16,30 @@ function Fastily() {
 
             <h1 className="page-title">fastily</h1>
 
-            <p className="text-justify pt-2">
+            <section className="my-2">
+                <div className="flex gap-1">
+                    <h5 className="m-0 text-black font-semibold text-[14px]">About:</h5>
+                    <p className="m-0 text-black">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.
+                    </p>
+                </div>
+
+                <div className="flex gap-1">
+                    <h5 className="m-0 text-black font-semibold text-[14px]">Technologies:</h5>
+                    <p className="m-0 text-black">Swift, SwiftUI, Firebase</p>
+                </div>
+
+                <div className="flex gap-1">
+                    <h5 className="m-0 text-black font-semibold text-[14px]">Timeline:</h5>
+                    <p className="m-0 text-black">2025–Present</p>
+                </div>
+            </section>
+
+            <p className="text-justify text-black my-2">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet diam semper tempor tincidunt. Proin ligula odio, congue a eros id, laoreet viverra mauris. Nullam sed elit eu orci euismod semper at at erat. Aliquam at sem urna. Fusce maximus scelerisque augue, a dignissim sapien varius sit amet. Donec gravida metus ac mollis cursus. Pellentesque molestie, mauris at mattis interdum, lectus metus accumsan sapien, sed fringilla leo lorem sed ex. Ut quis placerat dolor. Phasellus nec nisl sed lectus hendrerit malesuada.
-                In dictum convallis est, quis tristique metus. Nunc ut velit convallis, posuere turpis ut, rutrum lacus. Integer a purus posuere, vehicula nibh quis, finibus ipsum. Fusce rutrum, risus vel congue auctor, urna magna ultrices urna, feugiat scelerisque metus lorem quis lectus. In augue nisi, convallis sit amet massa quis, dignissim aliquam nisi. Nam ultrices convallis libero. Sed gravida sem sit amet metus efficitur, quis tempor ligula faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec in fermentum leo. Aenean malesuada, est a cursus sodales, odio eros ultrices leo, eget facilisis risus arcu ac turpis. Aenean suscipit malesuada nunc, quis tincidunt erat imperdiet at. Cras ultrices blandit nunc a dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
             </p>
-            
-            <div className="columns-2 gap-4">
+
+            <div className="columns-2 gap-4 my-4">
                 <img className="mb-4 w-full" src={image1} alt="fastily" />
                 <img className="mb-4 w-full" src={image2} alt="fastily" />
                 <img className="mb-4 w-full" src={image1} alt="fastily" />

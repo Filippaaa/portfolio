95ade0c1a48f6b34744031c0b1284e795c981d1c
Filippa <filippa.steiner@icloud.com>
2026-09-05 22:27:13 +0200

    p

--- src/pages/Fastily.tsx
+++ src/pages/Fastily.tsx
@@ -1,27 +1,35 @@
 import { ArrowLeft } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
+import image1 from '../assets/portrait.jpg'
+import image2 from '../assets/fastily/Logo.png'
+
 function Fastily() {
-  return (
-    <div className="relative mt-[100px] mb-5">
-      
-      <Link
-        to="/"
-        className="absolute -left-12 top-1"
-      >
-        <ArrowLeft size={18} strokeWidth={1.5} />
-      </Link>
+    return (
+        <div className="relative mt-[100px] mb-5">
 
-      <h1 className="page-title">fastily</h1>
+            <Link
+                to="/"
+                className="absolute -left-12 top-1"
+            >
+                <ArrowLeft size={18} strokeWidth={1.5} />
+            </Link>
 
-      <p className="text-justify pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet diam semper tempor tincidunt. Proin ligula odio, congue a eros id, laoreet viverra mauris. Nullam sed elit eu orci euismod semper at at erat. Aliquam at sem urna. Fusce maximus scelerisque augue, a dignissim sapien varius sit amet. Donec gravida metus ac mollis cursus. Pellentesque molestie, mauris at mattis interdum, lectus metus accumsan sapien, sed fringilla leo lorem sed ex. Ut quis placerat dolor. Phasellus nec nisl sed lectus hendrerit malesuada.
+            <h1 className="page-title">fastily</h1>
 
-In dictum convallis est, quis tristique metus. Nunc ut velit convallis, posuere turpis ut, rutrum lacus. Integer a purus posuere, vehicula nibh quis, finibus ipsum. Fusce rutrum, risus vel congue auctor, urna magna ultrices urna, feugiat scelerisque metus lorem quis lectus. In augue nisi, convallis sit amet massa quis, dignissim aliquam nisi. Nam ultrices convallis libero. Sed gravida sem sit amet metus efficitur, quis tempor ligula faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec in fermentum leo. Aenean malesuada, est a cursus sodales, odio eros ultrices leo, eget facilisis risus arcu ac turpis. Aenean suscipit malesuada nunc, quis tincidunt erat imperdiet at. Cras ultrices blandit nunc a dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-
-</p>
-
-    </div>
-  )
+            <p className="text-justify pt-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet diam semper tempor tincidunt. Proin ligula odio, congue a eros id, laoreet viverra mauris. Nullam sed elit eu orci euismod semper at at erat. Aliquam at sem urna. Fusce maximus scelerisque augue, a dignissim sapien varius sit amet. Donec gravida metus ac mollis cursus. Pellentesque molestie, mauris at mattis interdum, lectus metus accumsan sapien, sed fringilla leo lorem sed ex. Ut quis placerat dolor. Phasellus nec nisl sed lectus hendrerit malesuada.
+                In dictum convallis est, quis tristique metus. Nunc ut velit convallis, posuere turpis ut, rutrum lacus. Integer a purus posuere, vehicula nibh quis, finibus ipsum. Fusce rutrum, risus vel congue auctor, urna magna ultrices urna, feugiat scelerisque metus lorem quis lectus. In augue nisi, convallis sit amet massa quis, dignissim aliquam nisi. Nam ultrices convallis libero. Sed gravida sem sit amet metus efficitur, quis tempor ligula faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec in fermentum leo. Aenean malesuada, est a cursus sodales, odio eros ultrices leo, eget facilisis risus arcu ac turpis. Aenean suscipit malesuada nunc, quis tincidunt erat imperdiet at. Cras ultrices blandit nunc a dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+            </p>
+            
+            <div className="columns-2 gap-4">
+                <img className="mb-4 w-full" src={image1} alt="fastily" />
+                <img className="mb-4 w-full" src={image2} alt="fastily" />
+                <img className="mb-4 w-full" src={image1} alt="fastily" />
+                <img className="mb-4 w-full" src={image1} alt="fastily" />
+            </div>
+        </div>
+    )
 }
 
 export default Fastily
